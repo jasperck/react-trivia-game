546d259c72fbc1327e6da44d83d8454f32eb0e0d
Jasper Chang <jasperc8@gmail.com>
2019-03-20 11:19:27 +0800

    some cleanup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
+import Players from "./Components/Players";
+import Play from "./Components/Play";
+import Quiz from "./Components/Quiz";
+import Game from "./Components/Game";
+import Container from "./Components/Container";
+import { getRandomQuiz } from './utils';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [gameStarted, updateGameStatus] = useState(false);
+  const [players, updatePlayers] = useState([]);
+  const [quiz, updateQuiz] = useState(getRandomQuiz());
+
+  const startTrivia = useCallback(() => {
+    updateGameStatus(true);
+
+    fetch("/api/send-sms", {
+      method: "POST",
+      body: JSON.stringify({ numbers: players, quiz, limitedTime: 30 }),
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+      .then(res => console.log(res, 4))
+      .catch(err => {
+        console.error(err);
+        updateGameStatus(false);
+      });
+  }, [players, quiz]);
+
+  return (
+    <Container>
+      <Game>
+        <Players players={players} updatePlayers={updatePlayers} />
+        <Quiz quiz={quiz} refreshQuiz={() => updateQuiz(getRandomQuiz())} />
+      </Game>
+      <Play onClick={startTrivia} disabled={!players.length || gameStarted}>Play!</Play>
+    </Container>
+  );
+};
 
 export default App;
