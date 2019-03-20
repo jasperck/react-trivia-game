595ac31b3e235b14b7ac6389f993f282990de8a6
Jasper Chang <jasperc8@gmail.com>
2019-03-20 11:19:45 +0800

    add quizzes

---
+++ src/quizzes.js
@@ -0,0 +1,34 @@
+const quizzes = [
+  {
+    question: 'In which year did the De Stijl movement originate?',
+    options: ['1987', '1917', '2007', '1947'],
+    answer: '1917',
+  },
+  {
+    question: 'What colour was the bed sheets in Roy Lichtenstein’s 1992 version of Bedroom at Arles?',
+    options: ['Red', 'Yellow', 'Black', 'Blue'],
+    answer: 'Red',
+  },
+  {
+    question: 'Who featured on the 2013 Will.I.Am hit “Willpower”?',
+    options: ['Bono', 'Adam Levine', 'Justin Bieber', 'Justin Timberlake'],
+    answer: 'Justin Bieber',
+  },
+  {
+    question: 'Which golfer has the nickname “Wild thing”?',
+    options: ['Fred Couples', 'John Daly', 'David Duval', 'Sam Snead'],
+    answer: 'John Daly',
+  },
+  {
+    question: 'In total how many pairs of ribs are there in the human body?',
+    options: ['6', '12', '24', '36'],
+    answer: '12',
+  },
+  {
+    question: 'Steve Jobs, Ronald Wayne and Steve Wozniak founded which company in 1976?',
+    options: ['Facebook', 'Google', 'Apple', 'Twilio'],
+    answer: 'Apple',
+  },
+];
+
+export default quizzes;
